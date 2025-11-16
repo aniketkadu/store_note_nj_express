79a9cee127210e3b393fa9f94985c358a5b7b7a7
aniket <aniketskadu@gmail.com>
2025-11-16 13:41:19 +0530

    db change updated

--- database.js
+++ database.js
@@ -2,12 +2,20 @@
 const mysql2 = require('mysql2');
 
 // Set up the connection pool
+// const pool = mysql2.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'ritZ#Mobile',
+//   database: 'store_note',
+//   connectionLimit: 10
+// });
+// Set up the connection pool
 const pool = mysql2.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'ritZ#Mobile',
-  database: 'store_note',
-  connectionLimit: 10
+   host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 // Export the pool for use in other files
