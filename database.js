2553bde9bc06393b789e8dcf2fc475b5e6813aed
aniket <aniketskadu@gmail.com>
2025-11-19 14:33:50 +0530

    db fix

--- database.js
+++ database.js
@@ -1,27 +1,24 @@
-// database.js
 const mysql2 = require('mysql2/promise');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const url = require('url');
 
 dotenv.config();
 
-//Set up the connection pool
-// const pool = mysql2.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'ritZ#Mobile',
-//   database: 'store_note',
-//   connectionLimit: 10
-// });
-// Set up the connection pool
+let config = {};
 
-const pool = mysql2.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  connectionLimit: 10
-});
+if (process.env.DB_URL) {
+  const dbUrl = new url.URL(process.env.DB_URL);
 
-// Export the pool for use in other files
+  config = {
+    host: dbUrl.hostname,
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.replace('/', ''),
+    port: dbUrl.port,
+    connectionLimit: 10
+  };
+}
+
+const pool = mysql2.createPool(config);
+
 module.exports = pool;
